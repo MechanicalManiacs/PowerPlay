8d74e9d959cd6f74fde502d771e8f5a1c18e684b
SarangaR <sarnga.raj@gmail.com>
2022-08-10 21:12:56 -0400

    install roadrunner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v3/robot/Fishlo.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.fishlo.v3.robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+public class Fishlo extends Robot {
+
+    public Fishlo(OpMode opMode) {
+        super(opMode);
+    }
+
+}
