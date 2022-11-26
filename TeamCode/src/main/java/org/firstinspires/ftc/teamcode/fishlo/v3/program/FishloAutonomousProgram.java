0384d94c72b8d08686541d1d99b7a22843e7c88f
SarangaR <sarnga.raj@gmail.com>
2022-11-26 15:56:02 -0500

    telop/test code; wrong branch, will fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v3/program/FishloAutonomousProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v3/program/FishloAutonomousProgram.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.fishlo.v3.program;
 
+import org.firstinspires.ftc.teamcode.fishlo.v3.robot.Drive;
 import org.firstinspires.ftc.teamcode.fishlo.v3.robot.Fishlo;
 import org.firstinspires.ftc.teamcode.fishlo.v3.robot.Vision;
 import org.firstinspires.ftc.teamcode.opMode.AutonomousProgram;
@@ -8,11 +9,13 @@ import org.firstinspires.ftc.teamcode.robot.Robot;
 public class FishloAutonomousProgram extends AutonomousProgram {
 
     protected Vision vision;
+    protected Drive drive;
 
     @Override
     protected Robot buildRobot() {
         Fishlo fishlo = new Fishlo(this);
         vision = (Vision) fishlo.getSubSystem("Vision");
+        drive = (Drive) fishlo.getSubSystem("Drive");
         return fishlo;
     }
 
