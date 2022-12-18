e6f0e106f71d4bbd2c295d7721f32c64190cc2e6
rahulsantha2006 <rahul.santha.2006@gmail.com>
2022-12-18 18:21:37 -0500

    move to cone, do task 3 time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v3/program/competition/RedLeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v3/program/competition/RedLeftAuto.java
@@ -8,12 +8,14 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.fishlo.v3.program.FishloAutonomousProgram;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
-
 @Autonomous
-public class EightTest extends FishloAutonomousProgram {
+public class RedLeftAuto extends FishloAutonomousProgram {
+
     SampleMecanumDrive mecanumDrive;
     Pose2d startPose;
     final int OFFSET = 7;
+    int temporal_marker;
+
     @Override
     protected Robot buildRobot() {
         return super.buildRobot();
@@ -22,18 +24,38 @@ public class EightTest extends FishloAutonomousProgram {
     @Override
     public void preMain() {
         mecanumDrive = new SampleMecanumDrive(hardwareMap);
-        startPose = new Pose2d(-72+OFFSET, 36, 0);
+        startPose = new Pose2d(-72, 36, 0);
     }
 
     @Override
     public void main() {
         mecanumDrive.setPoseEstimate(startPose);
         TrajectorySequence sequence = mecanumDrive.trajectorySequenceBuilder(startPose)
-                .splineToConstantHeading(new Vector2d(-48, 12), Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(-48+24, 12+24), Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(-48+24+24, 12+24-24), Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(-48+24+24+24, 12+24-24+24), Math.toRadians(0))
+                .lineTo(new Vector2d(-30,36))
+                .splineTo(new Vector2d(-18, 55), Math.toRadians(90))
+                //1st CONE
+                .lineTo(new Vector2d(-18, 30))
+                .UNSTABLE_addDisplacementMarkerOffset(0, () -> {
+                    //move arm
+                })
+                .lineTo(new Vector2d(-18, 55))
+
+                //2nd CONE
+                .lineTo(new Vector2d(-18, 30))
+                .UNSTABLE_addDisplacementMarkerOffset(0, () -> {
+                    //move arm
+                })
+                .lineTo(new Vector2d(-18, 55))
+
+                //3rd CONE
+                .lineTo(new Vector2d(-18, 30))
+                .UNSTABLE_addDisplacementMarkerOffset(0, () -> {
+                    //move arm
+                })
+                .lineTo(new Vector2d(-18, 55))
+
                 .build();
         mecanumDrive.followTrajectorySequence(sequence);
     }
 }
+
